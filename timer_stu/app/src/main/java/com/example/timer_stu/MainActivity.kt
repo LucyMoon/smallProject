79582310b5c36e6673b94bd051a2c585192fb575
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2022-03-08 16:08:03 +0900

    Timer
    2022-03-08
    중학생에게 안드로이드 강의를 할 때 가르쳐 줄 프로젝트를 미리 만들어 보았다

---
+++ timer_stu/app/src/main/java/com/example/timer_stu/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.timer_stu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
+
+class MainActivity : AppCompatActivity() {
+    private var check = false
+    private var time = 0
+    private var timerTask : Timer? = null
+    private var timetv : TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        timetv = findViewById(R.id.time_tv)
+        val recordbtn = findViewById<Button>(R.id.record)
+        val startbtn = findViewById<Button>(R.id.start)
+        val stopbtn = findViewById<Button>(R.id.stop)
+        val resetbtn = findViewById<ImageView>(R.id.refresh)
+        val list_tv = findViewById<TextView>(R.id.list_tv)
+        startbtn.setOnClickListener{
+            if(!check) {
+                startTimer()
+            }
+        }
+        stopbtn.setOnClickListener {
+            if(check) {
+                stopTimer()
+            }
+        }
+        resetbtn.setOnClickListener{
+            resetTimer()
+        }
+        recordbtn.setOnClickListener{
+            var sec = time / 100
+            var milli = time % 100
+            var min = time / 6000
+            list_tv.text = "${list_tv.text.toString()}\n$min : $sec : $milli"
+        }
+
+
+    }
+    private fun startTimer(){
+        check = true
+        timerTask = timer(period = 10){
+            time++
+
+            var sec = time / 100
+            var milli = time % 100
+            var min = time / 6000
+            var empty = if(milli < 10) "0"
+            else ""
+
+            runOnUiThread{
+                timetv!!.text = "$min : $sec : $milli$empty"
+            }
+        }
+    }
+    private fun stopTimer(){
+        check = false
+        timerTask?.cancel()
+    }
+    private fun resetTimer(){
+        if(check) {
+            check = false
+            timerTask?.cancel()
+        }
+        findViewById<TextView>(R.id.list_tv).text = ""
+        time = 0
+        timetv!!.text = "0 : 0 : 00"
+    }
+}
