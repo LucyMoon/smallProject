dd5def11afc05b1e6a424ae484381bd88f997ad4
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2022-03-22 11:34:28 +0900

    BottomnaviwithFloating
    2022-03-22
    바텀 네비게이션 안에 플로팅 버튼을 넣는 방법을 배웠다

---
+++ BottomnaviwithFloating/app/src/main/java/com/example/bottomnaviwithfloating/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bottomnaviwithfloating
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+    }
+}
